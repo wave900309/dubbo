6b4bb97e85a30fab93b6d3617d8ce1053431147d
wave900309 <wave900309@gmail.com>
2018-06-12 15:53:54 +0800

    project parent name change

--- consumer/src/main/java/net/ken/util/Launcher.java
+++ consumer/src/main/java/net/ken/util/Launcher.java
@@ -3,16 +3,21 @@ package net.ken.util;
 import net.ken.service.DemoService;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by Yang, Haiqiang on 2018/05/16.
  */
 public class Launcher {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 new String[] { "classpath:consumer.xml" });
         context.start();
         DemoService demoService = (DemoService) context.getBean("demoService");
-        System.out.println(demoService.foo());
+        while (System.in.read() == 1){
+            String foobar = demoService.foo();
+            System.out.print(foobar);
+        }
     }
 }
---
+++ consumer/src/main/java/net/ken/util/M.java
@@ -0,0 +1,14 @@
+package net.ken.util;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Yang, Haiqiang on 2018/05/21.
+ */
+public class M {
+    public static void main(String[] args) {
+
+    }
+}
