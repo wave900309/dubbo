689fcc5ba1b82957ad738d28096bd25d966e32de
wave900309 <wave900309@gmail.com>
2018-10-19 15:23:02 +0800

    change

--- spring/src/main/resources/ss.sh
+++ spring/src/main/resources/ss.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-yum update && yum -y install epel-release && yum install python-pip && pip install shadowsocks && echo '{"server":"0.0.0.0","server_port":9007,"local_address":"127.0.0.1","local_port":1080,"password":"unblock","timeout":300,"method":"aes-256-cfb","fast_open":false}'
+yum update && yum -y install epel-release && yum install python-pip && pip install shadowsocks && echo '{"server":"0.0.0.0","server_port":9007,"local_address":"127.0.0.1","local_port":1080,"password":"unblock","timeout":500,"method":"aes-256-cfb","fast_open":false}' > /etc/shadowsocks.json && ssserver -c /etc/shadowsocks.json -d start
